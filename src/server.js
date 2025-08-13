d46977811e30be38c90537a7b39bfa94558c7206
Jhonautas <cassio.cruz@ifsp.aluno.edu.br>
2025-08-13 13:28:31 -0300

    Criado o crud do profile

--- src/server.js
+++ src/server.js
@@ -1,12 +1,34 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+
+app.use(express.json()
+)
+app.get("/profile", (req, res) => {
+    console.log("Dados do usúario coletados com sucesso!");
+     res.json({ profile:"Informações coletadas com sucesso!" });
+});
+
+app.post("/profile", (req, res) => {
+    console.log(dados);
+     res.json({ profile: "Usuário criado com sucesso!" });
+});
+
+app.put("/profile", (req, res) => {
+    const dados = req.body 
+    console.log("Dados do usuário atualizados com sucesso");
+    res.json({ 
+      profile: "Dados do usuário com editado com sucesso!",
+      profile: dados });
+});
+
+app.delete("/profile", (req, res) => {
+    console.log("Usuário Deletado com sucesso");
+    res.json({ profile: "Usuário deletado com sucesso!" });
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port http://localhost:${port}`)
-})
+    console.log(`Api criada com sucesso http://localhost:${port}`);
+});
